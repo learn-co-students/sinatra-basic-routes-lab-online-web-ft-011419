4840dc2e31b08e58c0b9db526530e5d530e33eff
mikeygbd <mikey.soares@gmail.com>
2019-02-19 22:52:49 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Mikey"
+  end
+  get '/hometown' do
+    "My hometown is Northport"
+  end
+  get '/favorite-song' do
+    "My favorite song is doses and mimosas"
+  end
 end
